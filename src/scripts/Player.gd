7d777865e2479a3abe91ed27c86460b2a5ced750
Rittmann <willirittmann@outlook.com>
2023-09-13 00:41:43 -0300

    Add more obstacles and add a initial impulse for the player
    Signed-off-by: Rittmann <willirittmann@outlook.com>

--- src/scripts/Player.gd
+++ src/scripts/Player.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+    apply_impulse(Vector2(0, -1500))
     pass # Replace with function body.
 
 
