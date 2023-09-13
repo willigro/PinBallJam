fd3bf0e07c9e2c5a14e2bdfbc3fbb259aeaf6737
Rittmann <willirittmann@outlook.com>
2023-09-13 16:32:01 -0300

    Reset player state and apply a force when click
    Signed-off-by: Rittmann <willirittmann@outlook.com>

--- src/scripts/Player.gd
+++ src/scripts/Player.gd
@@ -1,8 +1,11 @@
 extends RigidBody2D
 
+var _initial_transform
+var reset_state_pos = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+    _initial_transform = transform
     apply_impulse(Vector2(0, -1500))
     pass # Replace with function body.
 
@@ -10,3 +13,13 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
     pass
+
+func reset_states(pos):
+    reset_state_pos = pos
+    
+func _integrate_forces(state):
+    if reset_state_pos:
+        state.transform = Transform2D(0.0, reset_state_pos)
+        linear_velocity = Vector2.ZERO
+        reset_state_pos = null
+        apply_impulse(Vector2(0, -1500))
--- src/scripts/SceneTest.gd
+++ src/scripts/SceneTest.gd
@@ -3,9 +3,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+    $Player1.set_global_position($Marker2D.get_global_position())
     pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+    if Input.is_action_pressed("click"):
+        $Player1.reset_states($Marker2D.get_global_position())
     pass
