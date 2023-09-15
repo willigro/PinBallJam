be70465f75e5961b5f62050c51b555d15836e1c5
noscire <erickxpaz@gmail.com>
2023-09-15 15:38:44 -0300

    Account for points when hitting obstacles

--- src/scripts/Obstacle.gd
+++ src/scripts/Obstacle.gd
@@ -8,34 +8,40 @@ enum Type {WEAK, NORMAL, STRONG}
 
 @onready var hit_audio_player = $AudioStreamPlayer2D
 
+var _point = 0
+
+signal point(arg)
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    match _type:
-        Type.WEAK:
-            _force_of_impulse = Vector2(300, 300)
-            $Sprite2D.modulate = Color(1, 1, 1)
-        Type.NORMAL:
-            _force_of_impulse = Vector2(500, 500)
-            $Sprite2D.modulate = Color(0, 0, 1)
-        Type.STRONG:
-            _force_of_impulse = Vector2(1000, 1000)
-            $Sprite2D.modulate = Color(1, 0, 0)
-    pass # Replace with function body.
+	match _type:
+		Type.WEAK:
+			_force_of_impulse = Vector2(300, 300)
+			$Sprite2D.modulate = Color(1, 1, 1)
+			_point = 10
+		Type.NORMAL:
+			_force_of_impulse = Vector2(500, 500)
+			$Sprite2D.modulate = Color(0, 0, 1)
+			_point = 20
+		Type.STRONG:
+			_force_of_impulse = Vector2(1000, 1000)
+			$Sprite2D.modulate = Color(1, 0, 0)
+			_point = 50
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    pass
+	pass
 
 
 func _on_area_2d_body_entered(body):
-    if(body.is_in_group("PLAYER")):
-        var look_vec = (body.global_position - global_position).normalized()
-        print(look_vec)
-        #var dir = atan2(look_vec.y, look_vec.x)
-        var vel = _force_of_impulse * look_vec
-        print("touch")
-        body.linear_velocity = vel
-        print(body.linear_velocity)
-        hit_audio_player.play()
-    pass # Replace with function body.
+	if(body.is_in_group("PLAYER")):
+		var look_vec = (body.global_position - global_position).normalized()
+		var vel = _force_of_impulse * look_vec
+		body.linear_velocity = vel
+		hit_audio_player.play()
+		
+		emit_signal("point", _point)
+		
+	pass # Replace with function body.
--- src/scripts/SceneTest.gd
+++ src/scripts/SceneTest.gd
@@ -4,25 +4,36 @@ var FORCE_FACTOR = 500
 var _initial_force = 300
 var _max_force = 1500
 var _current_force = _initial_force
+var _count = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    $Player1.set_global_position($Marker2D.get_global_position())
-    pass # Replace with function body.
+	$Player1.set_global_position($Marker2D.get_global_position())
+	
+	for object in $Node2D.get_children():
+		object.point.connect(add_point)
+	
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    if Input.is_action_just_pressed("space"):
-        $Player1.reset_states($Marker2D.get_global_position())
-        
-    if Input.is_action_pressed("click"):
-        _current_force += FORCE_FACTOR * delta
-        if _current_force > _max_force:
-            _current_force = _max_force
-        print(_current_force)
-    
-    if Input.is_action_just_released("click"):
-        $Player1.push(Vector2(0, -_current_force))
-        _current_force = _initial_force
-    pass
+	if Input.is_action_just_pressed("space"):
+		$Player1.reset_states($Marker2D.get_global_position())
+		
+	if Input.is_action_pressed("click"):
+		_current_force += FORCE_FACTOR * delta
+		if _current_force > _max_force:
+			_current_force = _max_force
+		print(_current_force)
+	
+	if Input.is_action_just_released("click"):
+		$Player1.push(Vector2(0, -_current_force))
+		_current_force = _initial_force
+	pass
+
+func add_point(point):
+	_count = _count + point
+	print(_count)
+	pass
+	
